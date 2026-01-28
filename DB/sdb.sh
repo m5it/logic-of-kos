594736e8289683303dbe091a9bdc3cb30175a010
t3ch <w4d4f4k@gmail.com>
2026-01-28 04:15:14 +0000

    db scripts. for syncing remote, local db

--- DB/sdb.sh
+++ DB/sdb.sh
@@ -1,68 +1,53 @@
 #!/bin/bash
+#--
+# sdb.* v0.1 by B.K aka t3ch -> w4d4f4k at gmail dot com
+# sdb.* Script to sync local with remote database!
+#--
+# Used files:
+# - sdb.config  ( configurations for remote and local db )
+# - sdb.sh      ( main script that do the job )
+# - sdbloop.sh  ( Define db names )
+
 #-- POSSIBLE ERRORS ON STEP 4:
 # mariadb-dump: Error: 'Access denied; you need (at least one of) the PROCESS privilege(s) for this operation' when trying to dump tablespaces
 # mariadb-dump: Got error: 1044: "Access denied for user 'pub4all'@'%' to database 'lokkal.com_myvents.photo_event'" when selecting the database
-#--
+
+#-- THINK ON PRIVILEGES IF ALL IS SET CORRECTLY if not OBSERVE DEBUG outputs!
 # GRANT PROCESS ON *.* TO 'youruser'@'%';
 # FLUSH PRIVILEGES
+# ---------------------------
+#       --== START ==--
 # ---------------------------
 # Script arguments
 # ---------------------------
 USE_SOURCE_DB=$1
 USE_DESTINATION_DB=$2
-# ---------------------------
-# Source Database Settings
-# ---------------------------
-SOURCE_USER=""
-SOURCE_PASS=""
-SOURCE_HOST=""
-SOURCE_PORT=""
-SOURCE_DB=""
-# ---------------------------
-# Destination Database Settings
-# ---------------------------
-DESTINATION_USER=""
-DESTINATION_PASS=""
-DESTINATION_HOST=""
-DESTINATION_PORT=""
-DESTINATION_DB=""
-a=[]
-# Read config file
-SOURCE_USER=$(cat sdb.config | awk -F'=' '/SOURCE_USER.*/{print $2}' | tr -d '"')
-SOURCE_PASS=$(cat sdb.config | awk -F'=' '/SOURCE_PASS.*/{print $2}' | tr -d '"')
-SOURCE_HOST=$(cat sdb.config | awk -F'=' '/SOURCE_HOST.*/{print $2}' | tr -d '"')
-SOURCE_PORT=$(cat sdb.config | awk -F'=' '/SOURCE_PORT.*/{print $2}' | tr -d '"')
-SOURCE_DB=$(cat sdb.config | awk -F'=' '/SOURCE_DB.*/{print $2}' | tr -d '"')
 #
-DESTINATION_USER=$(cat sdb.config | awk -F'=' '/DESTINATION_USER.*/{print $2}' | tr -d '"')
-DESTINATION_PASS=$(cat sdb.config | awk -F'=' '/DESTINATION_PASS.*/{print $2}' | tr -d '"')
-DESTINATION_HOST=$(cat sdb.config | awk -F'=' '/DESTINATION_HOST.*/{print $2}' | tr -d '"')
-DESTINATION_PORT=$(cat sdb.config | awk -F'=' '/DESTINATION_PORT.*/{print $2}' | tr -d '"')
-DESTINATION_DB=$(cat sdb.config | awk -F'=' '/DESTINATION_DB.*/{print $2}' | tr -d '"')
+COUNT_SYNCED=0
+COUNT_N=0
+#
+source sdb.config
 #
 if [[ "$SOURCE_DB" == "" ]]; then
 	echo "exit"
 	exit
 fi
-#
-COUNT_SYNCED=0
-COUNT_N=0
 #--
 # 
 if [[ "USE_SOURCE_DB" != "" ]]; then
 	SOURCE_DB=$USE_SOURCE_DB;
-	echo "Using SOURCE_DB: "$SOURCE_DB
+	if [[ $DEBUG == true ]]; then echo "Using SOURCE_DB: "$SOURCE_DB; fi
 fi
 if [[ "USE_DESTINATION_DB" != "" ]]; then
 	DESTINATION_DB=$USE_DESTINATION_DB;
-	echo "Using DESTINATION_DB: "$DESTINATION_DB
+	if [[ $DEBUG == true ]]; then echo "Using DESTINATION_DB: "$DESTINATION_DB; fi
 fi
 
 # ---------------------------
 # Get list of tables from source DB
 TABLES=$(mariadb -h "$SOURCE_HOST" -u "$SOURCE_USER" -p"$SOURCE_PASS" -P$SOURCE_PORT -N -s -e "use $SOURCE_DB;show tables;" --skip-ssl)
 for T in $TABLES; do
-	echo $COUNT_N".) Table: "$T
+	if [[ $DEBUG==true ]]; then echo $COUNT_N".) Table: "$T; fi
 	COUNT_N=$((COUNT_N+1))
 	# ---------------------------
 	# Step 1: Get auto-increment column name from source
@@ -73,34 +58,34 @@ for T in $TABLES; do
 	    AND TABLE_NAME = '$T' 
 	    AND EXTRA LIKE '%auto_increment%'
 	" --skip-ssl)
-
-	echo "Auto-increment column: $AC"
-	
 	# ---------------------------
 	# Step 2: Handle missing auto-increment column
 	if [ -z "$AC" ]; then
 	  echo "Warning: No auto-increment column found in table '$T'."
 	  continue
 	fi
-
 	# ---------------------------
 	# Step 3: Get last ID from source
 	SOURCE_LAST_ID=$(mariadb -u "$SOURCE_USER" -p"$SOURCE_PASS" "$SOURCE_DB" -h "$SOURCE_HOST" -P "$SOURCE_PORT" -Nse "SELECT MAX($AC) FROM $T" --skip-ssl)
 	DESTINATION_LAST_ID=$(mariadb -u "$DESTINATION_USER" -p"$DESTINATION_PASS" "$DESTINATION_DB" -h "$DESTINATION_HOST" -P "$DESTINATION_PORT" -Nse "SELECT MAX($AC) FROM $T" --skip-ssl)
 	#
 	if [[ "$SOURCE_LAST_ID" == "NULL" ]]; then
-		echo "Continuing.. Null."
+		if [[ $DEBUG == true ]]; then echo "Continuing.. Null."; fi
 		continue
 	fi
 	if [[ $SOURCE_LAST_ID -eq $DESTINATION_LAST_ID ]]; then
-		echo "Continuing.. Same ids."
+		if [[ $DEBUG == true ]]; then echo "Continuing.. Same ids."; fi
 		continue
 	fi
-	echo "Starting sync on table $T"
-	echo "Auto increment id: "$AC
-	echo "Source Last ID: "$SOURCE_LAST_ID
-	echo "Destination Last ID: "$DESTINATION_LAST_ID
-	echo "-----------------------------"
+	#
+	if [[ $DEBUG == true ]]; then
+		echo "-----------------------------"
+		echo "Starting sync on table $T"
+		echo "Auto increment id: "$AC
+		echo "Source Last ID: "$SOURCE_LAST_ID
+		echo "Destination Last ID: "$DESTINATION_LAST_ID
+		echo "-----------------------------"
+	fi
 	# ---------------------------
 	# Step 4: Sync data to destination using destination credentials
 	#--
@@ -109,6 +94,6 @@ for T in $TABLES; do
 	COUNT_SYNCED=$((COUNT_SYNCED+1))
 done
 
-echo "Ended..., synced "$COUNT_SYNCED
+echo "Ended... It Is Synced. Your day is saved *** "$COUNT_SYNCED
 
 
--- DB/sdbloop.sh
+++ DB/sdbloop.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
-# sdbloop.sh for sdb.sh
-# Define multiple databases that should be synced.
-#-------------------------------------------------
-# By B.K. - t3ch - w4d4f4k at gmail dot com
-#-------------------------------------------------
-DBS=('dlokkal.com_myvents:lokkal.com_myvents' 'dlokkal.com_thevents:lokkal.com_thevents' 'zocalodigital2:lokkal.com')
+#--
+# sdb.* v0.1 by B.K aka t3ch -> w4d4f4k at gmail dot com
+# sdb.* Script to sync local with remote database!
+#--
+# Used files:
+# - sdb.config  ( configurations for remote and local db )
+# - sdb.sh      ( main script that do the job )
+# - sdbloop.sh  ( Define db names )
+#--
+source sdb.config
+#
 for DB in ${DBS[@]}; do
 	DDB=${DB#*:}
 	SDB=${DB%%:*}
@@ -12,9 +17,5 @@ for DB in ${DBS[@]}; do
 	echo "DDB: $DDB"
 	#
 	./sdb.sh "$SDB" "$DDB"
-	#if [[ "$tmp" == "exit" ]]; then
-	#	echo "Something is not right with sdb.config. Exiting."
-	#	break
-	#fi
 done
 echo "Done!"
