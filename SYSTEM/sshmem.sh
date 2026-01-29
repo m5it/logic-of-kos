abf87ec795abd5e8528f4c3292dbd9c0c9fcae32
t3ch <w4d4f4k@gmail.com>
2026-01-29 11:59:52 +0000

    added sshmem

---
+++ SYSTEM/sshmem.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Script sshmem.sh use ssh-agent and ssh-add to remember your password when using git push or git pull and similar ssh things.
+#--
+# by B.K. aka t3ch - w4d4f4k at gmail dot com
+#--
+# MOTO: If is possible make it simpler... ;)
+#--
+eval $(ssh-agent -s)
+sleep 1
+ssh-add
