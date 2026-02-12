0a0bab0e58184459d089f4560098dfdb6f967bb8
t3ch at kosgen2 <w4d4f4k@gmail.com>
2026-02-12 05:31:10 +0000

    updates for backup..

--- FW/ipt_del_drops.sh
+++ FW/ipt_del_drops.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-#--
-# Script to delete DROPped ips or ranges from iptables
-# by t3ch
-#--
-FILE="dropped.txt"
-while read -r src; do
-    echo "Unblocking: $src"
-    iptables -D INPUT $src -j DROP
-done < $FILE
+
+if [[ -t 0 ]]; then
+	echo "Not piped!"
+else
+	echo "Piped!"
+fi
