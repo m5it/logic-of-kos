3d9814bc55c4969cb4a99ee3d59a4e3b6596a268
t3ch <w4d4f4k@gmail.com>
2026-04-16 14:18:24 +0100

    vmlist update

--- SYSTEMD/vmlist.sh
+++ SYSTEMD/vmlist.sh
@@ -41,7 +41,70 @@ get_image_size() {
 	echo "-"
 }
 
-if [[ "$1" == "-s" || "$1" == "--size" ]]; then
+get_loop_info() {
+	local img="$1"
+	local target=""
+	local found=false
+	
+	for dir in "/var/lib/machines" "/var/lib/machines/img" "/home/chroots"; do
+		if [[ -L "$dir/$img" ]]; then
+			target=$(readlink -f "$dir/$img" 2>/dev/null)
+			[[ -f "$target" ]] && found=true && break
+		elif [[ -f "$dir/$img" ]]; then
+			target="$dir/$img"
+			found=true
+			break
+		fi
+	done
+	
+	if ! $found; then
+		for dir in "/var/lib/machines" "/var/lib/machines/img" "/home/chroots"; do
+			if [[ -L "$dir/${img}.raw" ]]; then
+				target=$(readlink -f "$dir/${img}.raw" 2>/dev/null)
+				[[ -f "$target" ]] && found=true && break
+			elif [[ -f "$dir/${img}.raw" ]]; then
+				target="$dir/${img}.raw"
+				found=true
+				break
+			fi
+		done
+	fi
+	
+	if $found && [[ -n "$target" ]]; then
+		losetup -l 2>/dev/null | grep "$target" | awk '{print $1, $5}'
+		return
+	fi
+	
+	echo "- -"
+}
+
+SHOW_SIZE=false
+SHOW_LOOP=false
+
+for arg in "$@"; do
+	case "$arg" in
+		-s|--size) SHOW_SIZE=true ;;
+		-l|--loop) SHOW_LOOP=true ;;
+		-a|--all) SHOW_SIZE=true; SHOW_LOOP=true ;;
+	esac
+done
+
+if $SHOW_SIZE && $SHOW_LOOP; then
+	while read -r line; do
+		[[ -z "$line" ]] && continue
+		img=$(echo "$line" | awk '{print $1}')
+		size=$(get_image_size "$img")
+		loop_info=$(get_loop_info "$img")
+		echo "$line $size $loop_info"
+	done < <(machinectl list --no-legend)
+elif $SHOW_LOOP; then
+	while read -r line; do
+		[[ -z "$line" ]] && continue
+		img=$(echo "$line" | awk '{print $1}')
+		loop_info=$(get_loop_info "$img")
+		echo "$line $loop_info"
+	done < <(machinectl list --no-legend)
+elif $SHOW_SIZE; then
 	while read -r line; do
 		[[ -z "$line" ]] && continue
 		img=$(echo "$line" | awk '{print $1}')
